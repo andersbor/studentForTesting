ccaf8ded2b37a7748bd846279bee13be3b83efd3
andersbor <anbo@easj.dk>
2017-08-18 10:32:03 +0200

    first commit

---
+++ StudentForTesting/StudentForTesting/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentForTesting
+{
+    public class Student
+    {
+        private string _name;
+
+        public string Name
+
+        {
+            get { return _name; }
+            set
+            {
+                if (value == null) throw new ArgumentNullException("name", "Name is null") ;
+                if (value.Length < 2) throw new ArgumentException("Name length must be at least 2 characters");
+                _name = value; }
+        }
+    }
+}
