742d7281d20c795dc40ebc51009807f6cf8fae44
andersbor <anbo@easj.dk>
2017-08-18 10:37:54 +0200

    Student class done, but not tested

--- StudentForTesting/StudentForTesting/Student.cs
+++ StudentForTesting/StudentForTesting/Student.cs
@@ -1,14 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StudentForTesting
 {
+
+    public enum Gender
+    {
+        Male, Female
+    }
+
     public class Student
     {
         private string _name;
+        private string _address;
+        private int _semester;
+
 
         public string Name
 
@@ -16,9 +21,32 @@ namespace StudentForTesting
             get { return _name; }
             set
             {
-                if (value == null) throw new ArgumentNullException("name", "Name is null") ;
+                if (value == null) throw new ArgumentNullException("name", "Name is null");
                 if (value.Length < 2) throw new ArgumentException("Name length must be at least 2 characters");
-                _name = value; }
+                _name = value;
+            }
         }
+
+        public string Address
+        {
+            get { return _address; }
+            set
+            {
+                if (value == null) throw new ArgumentNullException("address", "Address is null");
+                _address = value;
+            }
+        }
+
+        public int Semester
+        {
+            get { return _semester; }
+            set
+            {
+                if (1 <= value && value <= 8) _semester = value;
+                else throw new ArgumentOutOfRangeException("semester", value, "Illegal semester");
+            }
+        }
+
+        public Gender Gender { get; set; }
     }
 }
